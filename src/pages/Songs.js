57b82f7cf50f513b8e950a6e7b9d5764840681d8
Bo Latt <bolattny@gmail.com>
2023-01-19 02:19:45 -0500

    render songs

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -2,8 +2,8 @@ import useFetch from "../hooks/useFetch";
 const url = process.env.REACT_APP_API_URL;
 
 function Songs() {
-  const { data, error, loading } = useFetch(url);
-  console.log(data, error, loading);
+  const { data: songs, error, loading } = useFetch(url);
+  console.log(songs, error, loading);
   return (
     <div className="container max-w-screen-xl mx-auto mt-5">
       <div className="overflow-x-auto  ">
@@ -19,24 +19,16 @@ function Songs() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
+            {songs.map((song, i) => (
+              <tr key={song.id}>
+                <th>{i + 1}</th>
+                <td>{song.name}</td>
+                <td>{song.artist}</td>
+                <td> {song.album}</td>
+                <td>{song.time}</td>
+                <td>{song.is_favorite + ""}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
