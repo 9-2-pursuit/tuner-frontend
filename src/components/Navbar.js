6a5becc14add5cf1b95d5fc73fc18f7c7aab54ab
Bo Latt <bolattny@gmail.com>
2023-01-20 05:37:13 -0500

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,43 +4,68 @@ import { MusicalNoteIcon } from "@heroicons/react/24/solid";
 function Navbar() {
   return (
     <div className="navbar bg-base-100 max-w-screen-xl mx-auto">
-      <div className="flex-1 text-violet-500	">
-        <Link to="/songs" className="btn btn-ghost normal-case text-3xl">
-          Tuner <MusicalNoteIcon className="w-7" />
-        </Link>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered"
-          />
-        </div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src="https://placeimg.com/80/80/people" alt="profile " />
-            </div>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </label>
           <ul
             tabIndex={0}
-            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
+              <a>Item 1</a>
+            </li>
+            <li tabIndex={0}>
               <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
+                Parent
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
               </a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
+              <a>Item 3</a>
             </li>
           </ul>
         </div>
+
+        <Link to="/songs" className="btn  btn-ghost normal-case text-3xl">
+          Tuner <MusicalNoteIcon className="w-7 text-violet-500" />
+        </Link>
+      </div>
+
+      <div className="navbar-end">
+        <Link to="songs/new" className="btn">
+          Add a new song
+        </Link>
       </div>
     </div>
   );
